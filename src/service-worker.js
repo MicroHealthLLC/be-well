9ff77b9799ad40f24ad99ae25c768eedd7e8eea9
Christopher Calderon <christopher.calderon@microhealthllc.com>
2021-12-27 15:27:25 -0800

    Fix query values

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,7 +2,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 self.addEventListener("notificationclick", (event) => {
-  console.log(event);
   event.notification.close();
   if (event.action === "view-content") {
     clients.openWindow(
@@ -10,6 +9,7 @@ self.addEventListener("notificationclick", (event) => {
         event.notification.data.category
       )}`
     );
+    console.log(event);
   } else if (event.action == "snooze") {
     console.log("SNOOZE");
   } else {
@@ -19,15 +19,15 @@ self.addEventListener("notificationclick", (event) => {
 
 function categoryQuery(key) {
   const categories = {
-    ENDURANCE: "Endurance",
-    ERGONOMICS: "Ergonomics",
-    MEDITATION: "Meditation",
-    MUSCLE: "Muscle Tone/Movement",
-    POSTURE: "Posture",
-    STRESS_RELIEF: "Stress Relief",
-    STRETCHING: "Stretching",
-    YOGA: "Yoga",
+    ENDURANCE: "endurance",
+    ERGONOMICS: "ergonomics",
+    MEDITATION: "meditation",
+    MUSCLE: "muscle-tone-movement",
+    POSTURE: "posture",
+    STRESS_RELIEF: "stress-relief",
+    STRETCHING: "stretching",
+    YOGA: "yoga",
   };
 
-  return categories[key];
+  return categories[key] || "endurance";
 }
