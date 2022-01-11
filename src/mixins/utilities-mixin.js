e76f7427dc6f0f150ef8603f1780e6bb7d007326
Christopher Calderon <christopher.calderon@microhealthllc.com>
2022-01-11 10:10:58 -0800

    Add image urls to utilities mixin

--- src/mixins/utilities-mixin.js
+++ src/mixins/utilities-mixin.js
@@ -7,48 +7,56 @@ export default {
           icon: "mdi-run",
           query: "endurance",
           value: "ENDURANCE",
+          image: "/img/endurance.jpg",
         },
         {
           title: "Ergonomics",
           icon: "mdi-seat-recline-extra",
           query: "ergonomics",
           value: "ERGONOMICS",
+          image: "/img/ergonomics.jpg",
         },
         {
           title: "Meditation",
           icon: "mdi-meditation",
           query: "meditation",
           value: "MEDITATION",
+          image: "/img/meditate.jpg",
         },
         {
           title: "Muscle Tone/Movement",
           icon: "mdi-weight-lifter",
           query: "muscle-tone-movement",
           value: "MUSCLE",
+          image: "/img/weight-training.jpg",
         },
         {
           title: "Posture",
           icon: "mdi-human-male",
           query: "posture",
           value: "POSTURE",
+          image: "/img/posture.jpg",
         },
         {
           title: "Stress Relief",
           icon: "mdi-head-heart",
           query: "stress-relief",
           value: "STRESS_RELIEF",
+          image: "/img/stress-relief.jpg",
         },
         {
           title: "Stretching",
           icon: "mdi-human",
           query: "stretching",
           value: "STRETCHING",
+          image: "/img/stretching.jpg",
         },
         {
           title: "Yoga",
           icon: "mdi-yoga",
           query: "yoga",
           value: "YOGA",
+          image: "/img/yoga.jpg",
         },
       ],
       levels: [
@@ -139,6 +147,11 @@ export default {
     },
     levelTitle(value) {
       return this.levels.find((level) => level.value == value).title || "";
+    },
+    imageURL(value) {
+      return (
+        this.categories.find((category) => category.value == value).image || ""
+      );
     },
   },
 };
