afd6f80f93ba5edba04b1b3a6c1a512262bb847a
Christopher Calderon <christopher.calderon@microhealthllc.com>
2022-01-24 15:15:42 -0800

    Initial homepage reconfigure

--- src/components/WelcomeBanner.vue
+++ src/components/WelcomeBanner.vue
@@ -1,17 +1,32 @@
 <template>
-  <v-sheet class="pa-5 welcome-card" dark app>
-    <p class="text-h5 font-weight-bold text-center">
-      Well-Beeing is here to help change your life for the better!
-    </p>
-    <p class="text-subtitle-2 text-center">
-      Whether reminding you to get moving, check your health, or take a mindful
-      moment, Well-Beeing promotes a healthy lifestyle.
-    </p>
-    <div class="d-flex justify-center">
-      <div class="mh-message text-caption text-sm-body-1">
-        Remember, a better YOU means a better MicroHealth!
-      </div>
-    </div>
+  <v-sheet class="welcome-card" dark app>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="6" order="2" order-sm="1">
+          <div class="d-flex flex-column justify-center fill-height">
+            <p class="text-h5 text-sm-h4 font-weight-bold text-center">
+              Well-Beeing is here to help change your life for the better!
+            </p>
+            <p class="text-subtitle-2 text-center">
+              Whether reminding you to get moving, check your health, or take a
+              mindful moment, Well-Beeing promotes a healthy lifestyle.
+            </p>
+            <div class="d-flex justify-center mb-5">
+              <div class="mh-message text-caption text-sm-body-1">
+                Remember, a better YOU means a better MicroHealth!
+              </div>
+            </div>
+          </div> </v-col
+        ><v-col cols="12" sm="6" order="1" order-sm="2">
+          <div class="d-flex justify-center">
+            <v-img
+              src="../assets/yoga-practice-bro.svg"
+              max-width="400"
+            ></v-img>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-sheet>
 </template>
 
