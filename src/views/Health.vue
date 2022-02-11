0a5d35ff2c9b8a2c2f9e803384949c90b1ddf84a
Christopher Calderon <christopher.calderon@microhealthllc.com>
2022-02-11 15:07:23 -0800

    Delete placeholder articles

--- src/views/Health.vue
+++ src/views/Health.vue
@@ -132,25 +132,6 @@
           </template>
         </v-data-table>
       </v-card>
-
-      <!-- Latest Articles -->
-      <span class="text-h6 text-sm-h5">Latest Articles</span>
-      <v-divider class="mb-4"></v-divider>
-
-      <div class="grid-container">
-        <v-card v-for="(article, index) in articles" :key="index" elevation="5">
-          <img class="image" :src="article.img" width="100%" />
-          <v-card-title>{{ article.title }}</v-card-title>
-          <v-card-subtitle>{{ article.author }}</v-card-subtitle>
-          <v-card-text>
-            {{ article.text }}
-          </v-card-text>
-          <v-card-actions class="align-end">
-            <v-btn text color="primary">View</v-btn>
-            <v-btn text color="primary">Add Reminder</v-btn>
-          </v-card-actions>
-        </v-card>
-      </div>
     </v-col>
   </v-row>
 </template>
@@ -207,26 +188,6 @@ export default {
           title: "USPSTF",
           link: "Preventative Screening Tool",
           href: "https://www.uspreventiveservicestaskforce.org/webview/#!/",
-        },
-      ],
-      articles: [
-        {
-          title: "Title 1",
-          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-          author: "First-name Last-name",
-          img: "/img/ergonomics.jpg",
-        },
-        {
-          title: "Title 2",
-          text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-          author: "First-name Last-name",
-          img: "/img/meditate.jpg",
-        },
-        {
-          title: "Title 3",
-          text: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          author: "First-name Last-name",
-          img: "/img/nutrition.jpg",
         },
       ],
     };
