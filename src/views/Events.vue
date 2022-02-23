31c71c9e216acb7909532b61e6e077a66ebf76e2
Christopher Calderon <christopher.calderon@microhealthllc.com>
2022-02-23 09:38:30 -0800

    Adjust mobile styles for Events

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,23 +1,48 @@
 <template>
   <v-row>
     <v-col>
-      <div class="text-h6 text-sm-h5 mt-2">Events</div>
+      <div class="text-h6 text-sm-h5 mt-2">Live Events</div>
       <v-divider class="mb-4"></v-divider>
       <!-- Events List -->
-      <v-card v-for="(event, index) in events" :key="index">
-        <v-card-title>{{ event.title }}</v-card-title>
-        <v-card-subtitle class="d-flex flex-column">
-          <div>
-            <v-chip class="mr-2" color="primary" small outlined
-              ><v-icon small left>mdi-human</v-icon>Category</v-chip
-            >
-            <v-chip color="primary" small outlined>{{
-              new Date(event.date).toDateString()
-            }}</v-chip>
-          </div>
-          <div>Hosted by: {{ event.hostName }}</div></v-card-subtitle
+      <v-card
+        v-for="(event, index) in events"
+        :key="index"
+        class="d-sm-flex flex-sm-row-reverse justify-sm-space-between mb-2"
+      >
+        <div
+          class="my-auto pa-sm-2"
+          :class="{ 'card-image': $vuetify.breakpoint.smAndUp }"
         >
-        <v-card-text>{{ event.description }}</v-card-text>
+          <v-img :src="cardImage(index)" aspect-ratio="1.5"></v-img>
+        </div>
+
+        <div
+          class="d-flex flex-column"
+          :class="{ 'card-info': $vuetify.breakpoint.smAndUp }"
+        >
+          <v-card-title>{{ event.title }}</v-card-title>
+          <v-card-subtitle class="d-flex flex-column">
+            <div class="text-caption mb-2">Hosted by: {{ event.hostName }}</div>
+            <div class="mb-2">
+              <v-chip class="mr-2" color="primary" small outlined
+                ><v-icon small left>{{ cardTypeIcon(event.type) }}</v-icon
+                >{{ event.type }}</v-chip
+              >
+              <v-chip color="primary" small outlined>{{
+                new Date(event.date).toDateString()
+              }}</v-chip>
+            </div></v-card-subtitle
+          >
+          <v-card-text
+            ><span class="clamp-text">{{
+              event.description
+            }}</span></v-card-text
+          >
+          <v-card-actions class="mt-auto pl-4 pb-4">
+            <v-btn outlined small>View Details</v-btn>
+            <v-btn outlined small>Join Event</v-btn>
+          </v-card-actions>
+        </div>
       </v-card>
     </v-col>
     <!-- Editor Button -->
@@ -40,6 +65,16 @@ export default {
   name: "Events",
   methods: {
     ...mapActions(["fetchEvents"]),
+    cardImage(index) {
+      return index == 0
+        ? "/img/nutrition.jpg"
+        : index == 1
+        ? "/img/posture.jpg"
+        : "/img/endurance.jpg";
+    },
+    cardTypeIcon(type) {
+      return type == "Live Virtual" ? "mdi-laptop" : "mdi-account-group";
+    },
   },
   computed: {
     ...mapGetters(["events", "isEditor"]),
@@ -57,5 +92,17 @@ export default {
   position: fixed;
   margin-right: 7vw;
   margin-bottom: 7vh;
+}
+.card-info {
+  width: 60%;
+}
+.card-image {
+  width: 40%;
+}
+.clamp-text {
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
 }
 </style>
