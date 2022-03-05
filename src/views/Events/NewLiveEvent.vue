e7ed3c8ec7bbeb3e905921aef7af64a344244ca9
Christopher Calderon <christopher.calderon@microhealthllc.com>
2022-03-04 17:17:22 -0800

    Replace v-row/v-col with div

--- src/views/Events/NewLiveEvent.vue
+++ src/views/Events/NewLiveEvent.vue
@@ -1,16 +1,14 @@
 <template>
-  <v-row>
-    <v-col>
-      <div class="d-flex justify-space-between mb-2 align-center">
-        <div class="text-h6 text-sm-h5">New Event</div>
-        <v-btn to="/events" exact-path color="#2f53b6" dark outlined small
-          ><v-icon>mdi-arrow-left</v-icon>Events</v-btn
-        >
-      </div>
-      <v-divider class="mb-4"></v-divider>
-      <EventForm />
-    </v-col>
-  </v-row>
+  <div>
+    <div class="d-flex justify-space-between mb-2 align-center">
+      <div class="text-h6 text-sm-h5">New Event</div>
+      <v-btn to="/events" exact-path color="#2f53b6" dark outlined small
+        ><v-icon>mdi-arrow-left</v-icon>Events</v-btn
+      >
+    </div>
+    <v-divider class="mb-4"></v-divider>
+    <EventForm />
+  </div>
 </template>
 
 <script>
