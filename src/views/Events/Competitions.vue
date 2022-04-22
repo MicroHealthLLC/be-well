19d31d4cf0b428ec6f9bc71a63ebefa0ed78bb85
Dimetros Seleshi <dimetros.seleshi@microhealthllc.com>
2022-04-22 11:49:34 -0400

    Placeholder for Competitions

--- src/views/Events/Competitions.vue
+++ src/views/Events/Competitions.vue
@@ -2,12 +2,28 @@
   <div>
     <div class="text-h6 text-sm-h5 mt-2 mb-2 mt-sm-4">Competitions</div>
     <v-divider class="mb-4"></v-divider>
+    <!-- Cometitions List -->
+    <div v-if="competitions.length > 0">
+      <CompetitionCard
+        v-for="(competition, index) in competitions"
+        :key="index"
+        :competition="competition"
+      />
+    </div>
+    <div v-else class="d-flex flex-column justify-center align-center py-10">
+      <div>
+        <v-icon class="mr-2">mdi-calendar-outline</v-icon> No Competitions...
+      </div>
+      <v-btn
+        v-if="isEditor"
+        to="/events/competitions/new"
+        class="mt-5"
+        color="primary"
+        text
+        >Add a New Competition</v-btn
+      >
+    </div>
 
-    <CompetitionCard
-      v-for="(competition, index) in competitions"
-      :key="index"
-      :competition="competition"
-    />
     <!-- Editor Button -->
     <v-btn
       v-if="isEditor"
