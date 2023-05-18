d697df7bdaf07f40751d0f055b9ca4903efeb57d
Katelyn Gomez <katelyn.gomez@microhealthllc.com>
2023-05-18 12:30:02 -0400

    Added GroupCard file

---
+++ src/components/GroupCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card
+    elevation="5"
+    max-width="375"
+  >
+    <v-card-title>{{ group.groupName }}</v-card-title>
+  </v-card>
+</template>
+
+<script>
+import dateMixin from "../mixins/date-mixin";
+
+export default {
+  name: "GroupCard",
+  mixins: [dateMixin],
+  props: {
+    group: {
+      type: Object,
+    },
+  },
+  methods: {
+    cardTypeIcon(type) {
+      return type == "Live Virtual" ? "mdi-laptop" : "mdi-account-group";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card-info {
+  width: 60%;
+}
+.card-image {
+  width: 40%;
+}
+.clamp-text {
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+}
+.rounded {
+  border-top-left-radius: inherit;
+  border-top-right-radius: inherit;
+  border-bottom-left-radius: revert !important;
+  border-bottom-right-radius: revert !important;
+}
+</style>
